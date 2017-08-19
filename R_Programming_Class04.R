c22687690a13e232f9697cd83dbe3b8077633f1e
FranklinRaymundo <f.raymundo.l@hotmail.com>
2017-08-19 17:43:07 -0500

    commit

--- R_Programming_Class04.R
+++ R_Programming_Class04.R
@@ -16,7 +16,24 @@
 # Data Frames #
 ###############
 
+# Crear un DataFrame
+
+# A partir de vectores
+
+edad <- c(10,15,20,25,40)
+peso <- c(55.5,45.6,78.0,65.2,58.6)
+talla <- c(1.60,1.71,1.75,1.75,1.65)
+
+alumnos <- data.frame(edad,peso,talla)
+
+alumnos <- data.frame(Age=edad,Weight=peso,Height=talla)
+alumnos
+
+
 # Importar un Data Frame
+
+# Working directory
+
 
 data1 <- read.csv('Fisrt_session.csv')
 head(data1)
